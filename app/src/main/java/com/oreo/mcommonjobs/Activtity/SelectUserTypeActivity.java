4a2d1635fb3daeed88d5d350f12cfbbf5a17092a
Kim <kimcodes@gmail.com>
2017-02-24 18:41:37 -0500

    setup select user type page

---
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/SelectUserTypeActivity.java
@@ -0,0 +1,15 @@
+package com.oreo.mcommonjobs.Activtity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.oreo.mcommonjobs.R;
+
+public class SelectUserTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user_type);
+    }
+}
