c62be46f6ce978a02ac7ca994b40220405591d96
jason <jtsalikis@hotmail.ca>
2017-02-26 16:47:22 -0500

    java logic for posting to db, need to add php file and create table

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/CreateJobPostAcitvity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/CreateJobPostAcitvity.java
@@ -5,12 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.oreo.mcommonjobs.R;
 public class CreateJobPostAcitvity extends AppCompatActivity {
 
     Spinner dropdown_menu_category;
+
+
+    String jobselected= "Painting Duties";
+    Button b;
+    TextView descption;
+    addJob addjob = new addJob(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,7 @@ public class CreateJobPostAcitvity extends AppCompatActivity {
         setContentView(R.layout.activity_create_job_post_acitvity);
 
         dropdown_menu_category = (Spinner)findViewById(R.id.spinner);
-        String[] menu_items = new String[] { "CATEGORY 1", "CATEGORY 2", "CATEGORY 3" };
+        String[] menu_items = new String[] { "Painting Duties", "Gardening Duties", "Vehicle Repair Duties", "Restaurant Duties","House Work Duties","Care Duties" };
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, menu_items);
@@ -27,19 +35,47 @@ public class CreateJobPostAcitvity extends AppCompatActivity {
         dropdown_menu_category.setAdapter(adapter);
         //dropdown_menu_category.setOnItemSelectedListener(this);
 
+        b=(Button) findViewById(R.id.btn_continue);
+        descption=(TextView) findViewById(R.id.textView);
+
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+            String type = "addjob";
+            String des = descption.getText().toString();
+
+                addjob.execute(type,jobselected,des);
+
+
+            }
+        });
+
+
+
     }
 
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
 
         switch (position) {
-            case 0:
-                // Whatever you want to happen when the first item gets selected
+            case 0: // Painting Duties
+                this.jobselected="Painting Duties";
                 break;
-            case 1:
-                // Whatever you want to happen when the second item gets selected
+            case 1: // Gardening Duties
+                this.jobselected="Gardening Duties";
                 break;
-            case 2:
-                // Whatever you want to happen when the thrid item gets selected
+            case 2: // Vehicle Repair Duties
+                this.jobselected="Vehicle Repair Duties";
+                break;
+            case 3: //Restaurant Duties
+                this.jobselected="Restaurant Duties";
+                break;
+            case 4: // House Work Duties
+                this.jobselected="House Work Duties";
+                break;
+            case 5: //Care Duties
+                this.jobselected="Care Duties";
                 break;
 
         }
