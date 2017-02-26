90adb269ff482d26a1cdb92da1c516c8e377b96c
jason <jtsalikis@hotmail.ca>
2017-02-26 02:15:00 -0500

    stuff works

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/SelectUserTypeActivity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/SelectUserTypeActivity.java
@@ -6,11 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.oreo.mcommonjobs.Controller.PersonController;
 import com.oreo.mcommonjobs.R;
 
 public class SelectUserTypeActivity extends AppCompatActivity {
 
     Button btn_jobseeker, btn_jobprovider;
+    String typeofuser;
+    RegisterAccount reg = new RegisterAccount(this);
+    PersonController instance = PersonController.getInstance();
+    String debugz;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +26,17 @@ public class SelectUserTypeActivity extends AppCompatActivity {
         btn_jobseeker = (Button) findViewById(R.id.btn_jobseeker);
         btn_jobprovider = (Button) findViewById(R.id.btn_jobprovider);
 
+
+
         btn_jobseeker.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
+                typeofuser = "jobseeker" ;
+
+                reg.execute("insert",instance.getFirstName(),instance.getLastName(),instance.getEmail(),typeofuser);
+
+                // add person to table in db, we get their info ffrom the personcontroller, and we also add weather tehy are job seeker or provider
+                // based off button they clicked
                 Intent i = new Intent(getApplicationContext(), NavigationActivity.class);
                 startActivity(i);
             }
@@ -31,6 +45,8 @@ public class SelectUserTypeActivity extends AppCompatActivity {
         btn_jobprovider.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
+                typeofuser = "jobprovider";
+                reg.execute("insert",instance.getFirstName(),instance.getLastName(),instance.getEmail(),typeofuser);
                 Intent i = new Intent(getApplicationContext(), NavigationActivity.class);
                 startActivity(i);
             }
