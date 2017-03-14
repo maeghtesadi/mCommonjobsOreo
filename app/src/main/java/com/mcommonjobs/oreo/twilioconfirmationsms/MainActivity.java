2683a3f53f3ded963620f4e5bd0e8b2dc97b30e8
sammoosavi <sammoosavi94@gmail.com>
2017-03-14 03:29:37 -0500

    OREO-48 #comment Activity that calls the Twilio php file for sending an sms confirmation code

---
+++ app/src/main/java/com/mcommonjobs/oreo/twilioconfirmationsms/MainActivity.java
@@ -0,0 +1,45 @@
+package com.mcommonjobs.oreo.twilioconfirmationsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText_confirmation;
+    private Button button_submit_confirmation;
+
+    private Random random;
+
+    private int inputConfirmation, generatedConfirmation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        random = new Random();
+        generatedConfirmation = random.nextInt(999999 - 100000) + 100000;
+        SmsActivity smsActivity = new SmsActivity(this);
+        smsActivity.execute(Integer.toString(generatedConfirmation));
+        editText_confirmation = (EditText) findViewById(R.id.editText_confirmation_number);
+        button_submit_confirmation = (Button) findViewById(R.id.button_submit_confirmation);
+        button_submit_confirmation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputConfirmation = Integer.getInteger(editText_confirmation.getText().toString());
+                if(inputConfirmation == generatedConfirmation){
+                    Toast.makeText(getApplicationContext(), "Confirmation codes matched!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Confirmation codes didn't match!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
