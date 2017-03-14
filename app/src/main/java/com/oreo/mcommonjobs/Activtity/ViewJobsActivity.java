eecb359d1ee960191f16725129037d14904ddc9c
jason <jtsalikis@hotmail.ca>
2017-03-14 14:02:57 -0400

    OREO-54

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/ViewJobsActivity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/ViewJobsActivity.java
@@ -28,19 +28,22 @@ import java.util.List;
  * Created by jason on 2017-03-12.
  */
 
+
+
+
 public class ViewJobsActivity extends AppCompatActivity {
 
     ListView jobslist;
     private List<Job> jobs = new ArrayList<>();
-    String debug = "hello";
+
     TextView t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_view_jobs);
 
-       // t = (TextView) (findViewById(R.id.hello)) ;
 
         engine();
 
@@ -48,138 +51,85 @@ public class ViewJobsActivity extends AppCompatActivity {
     }
 
 
+    private void engine() {
 
- private void engine(){
+        String url = "http://192.168.0.104/getjobs.php";
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
 
+                        try {
 
-/*
-    // String url ="http://192.175.117.182/test.php";
-     String url ="http://192.168.0.104/getjobs.php";
-
-     // Request a json response from the provided URL.
-     JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-             new Response.Listener<JSONObject>() {
-                 @Override
-                 public void onResponse(JSONObject response) {
-                debug="lol";
-                     // Display the first 500 characters of the response string.
-                     t.setText("Response is: "+ response.toString());
-                 }
-             }, new Response.ErrorListener() {
-         @Override
-         public void onErrorResponse(VolleyError error) {
-             t.setText("That didn't work!");
-         }
-     });
-     // Add the request to the RequestQueue.
-     */
-    // RequestSingleton.getInstance(this).addToRequestQueue(jsonRequest);
+                            JSONArray jobz = response.getJSONArray("jobs");
 
 
+                            for (int i = 0; i < jobz.length(); i++) {
+                                JSONObject temp = jobz.getJSONObject(i);
+
+                                String des = temp.getString("description");
+                                String typeofjob = temp.getString("typeofjob");
 
 
+                                jobs.add(new Job(des, typeofjob));
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
 
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RequestSingleton.getInstance(this).addToRequestQueue(jsonRequest);
 
 
+        ArrayAdapter<Job> adapter = new customAdapter();
+
+        ListView jobslist = (ListView) (findViewById(R.id.joblist));
+        jobslist.setAdapter(adapter);
 
 
-
-
-
-
-
-
-     //  String url = "http://127.0.0.1/getjobs.php";
-     String url = "http://192.168.0.104/getjobs.php";
-    //  String url = "http://localhost/getjobs.php";
-
-     //String url ="http://192.175.117.182/test.php";
-     JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-             new Response.Listener<JSONObject>() {
-                 @Override
-                 public void onResponse(JSONObject response) {
-                     //debug=response.toString();
-                     debug="i am here";
-                     try {
-
-                         JSONArray jobz = response.getJSONArray("jobs");
-
-
-                         for (int i = 0; i < jobz.length(); i++) {
-                             JSONObject temp = jobz.getJSONObject(i);
-
-                             String des = temp.getString("description");
-                             String typeofjob = temp.getString("typeofjob");
-
-
-                             jobs.add(new Job(des, typeofjob));
-
-                         }
-                     } catch (JSONException e) {
-                         e.printStackTrace();
-                     }
-                    // t.setText("Response is: "+ response.toString());
-
-                 } //hello
-             }, new Response.ErrorListener() {
-         @Override
-         public void onErrorResponse(VolleyError error) {
-             //t.setText("That didn't work!");
-         }
-     });
-
-     RequestSingleton.getInstance(this).addToRequestQueue(jsonRequest);
-
-
-     ArrayAdapter<Job> adapter = new customAdapter();
-
-     ListView jobslist = (ListView) (findViewById(R.id.joblist));
-     jobslist.setAdapter(adapter);
-
-
- }
-
-
-private class customAdapter extends ArrayAdapter<Job>{
-
-
-    public customAdapter() {
-        super(ViewJobsActivity.this, R.layout.jobfragment, jobs);
     }
 
-    @NonNull
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
 
-        if(convertView == null) {
-            convertView = getLayoutInflater().inflate(R.layout.jobfragment, parent, false);
+    private class customAdapter extends ArrayAdapter<Job> {
+
+
+        public customAdapter() {
+            super(ViewJobsActivity.this, R.layout.jobfragment, jobs);
         }
 
-        Job currentJob = jobs.get(position);
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
 
-       // ImageView newsImage = (ImageView) convertView.findViewById(R.id.leftIco);
-        TextView heading = (TextView) convertView.findViewById(R.id.heading);
-        TextView desc = (TextView) convertView.findViewById(R.id.desc);
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.jobfragment, parent, false);
+            }
 
+            Job currentJob = jobs.get(position);
 
-
-        heading.setText(currentJob.getCategory());
-        desc.setText(currentJob.getDescription());
-
-
-
-
-
-        return convertView;
-        //   return super.getView(position, convertView, parent);
+            // ImageView newsImage = (ImageView) convertView.findViewById(R.id.leftIco);
+            TextView heading = (TextView) convertView.findViewById(R.id.heading);
+            TextView desc = (TextView) convertView.findViewById(R.id.desc);
 
 
+            heading.setText(currentJob.getCategory());
+            desc.setText(currentJob.getDescription());
 
+
+            return convertView;
+
+
+        }
     }
-}
-
-
 
 
 }
