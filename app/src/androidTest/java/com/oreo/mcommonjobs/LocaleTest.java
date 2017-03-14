f82440c4413fc97f2906fa9938e48123661e1728
Kim Noel <kimcodes@gmail.com>
2017-03-14 16:41:49 -0400

    OREO-45 #comment added javadoc to test class LocaleTest

--- app/src/androidTest/java/com/oreo/mcommonjobs/LocaleTest.java
+++ app/src/androidTest/java/com/oreo/mcommonjobs/LocaleTest.java
@@ -3,9 +3,7 @@ package com.oreo.mcommonjobs;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.AndroidTestCase;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,21 +13,34 @@ import java.util.Locale;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.InstrumentationRegistry.getTargetContext;
-import static com.oreo.mcommonjobs.Activtity.SignInActivity.getContext;
 import static junit.framework.Assert.assertEquals;
 
 /**
- * Created by Rameen on 3/13/2017.
+ * This test class tests the multi-language support feature.
+ *
+ * @author Rameen
+ * @author kimcodes
  */
 @RunWith(AndroidJUnit4.class)
 public class LocaleTest {
     private Context context;
+    private final String LOCALE_ENGLISH_STRING = "en";
+    private final String LOCALE_FRENCH_STRING = "fr";
 
+
+    /**
+     * Setup method to run before every test to set the target context.
+     */
     @Before
     public void setup(){
         context = getTargetContext();
-
     }
+
+    /**
+     * This method sets the phone to the designated language.
+     *
+     * @param language  name of the language to set the locale to
+     */
     private void setLocale(String language) {
         //sets locale to be tested
         Locale locale = new Locale(language);
@@ -43,25 +54,26 @@ public class LocaleTest {
         res.updateConfiguration(config, res.getDisplayMetrics());
     }
 
+    /**
+     * Test - setting phone locale to English.
+     * @result the application will display strings in the appropriate language
+     */
     @Test
     public void testEnglishLocale() {
-        //set locale to EN (english)
-        setLocale("en");
-
-        //get profile string from strings.xml
+        setLocale(LOCALE_ENGLISH_STRING);
         String profileString = getTargetContext().getString(R.string.main_menu_profile_title);
         assertEquals("Profile", profileString);
     }
 
+    /**
+     * Test - setting phone locale to French.
+     * @result the application will display strings in the appropriate language
+     */
     @Test
     public void testFrenchLocale() {
-        //set locale to FR (french)
-        setLocale("fr");
-
-
+        setLocale(LOCALE_FRENCH_STRING);
         String profileString = getTargetContext().getString(R.string.main_menu_profile_title);
         assertEquals("Profil", profileString);
     }
-
 
 }
