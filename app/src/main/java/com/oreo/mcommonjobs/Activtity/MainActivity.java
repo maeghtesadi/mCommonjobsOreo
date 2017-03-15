df9e61e48a70f8718e9712cad7737bd6c2e186d7
Kim Noel <kimcodes@gmail.com>
2017-03-15 18:43:54 -0400

    javadoc done for main activity

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/MainActivity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/MainActivity.java
@@ -3,16 +3,28 @@ package com.oreo.mcommonjobs.Activtity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import com.oreo.mcommonjobs.R;
 
+/**
+ * Main Acitvity for the mCommonJobs application
+ * Launches startActivity to begin application.
+ *
+ * @author Jason
+ * @author Rameen
+ * @author kimcodes
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Method initializes main activity to launch the startActivity for a user to sign in.
+     * 
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         startActivity(new Intent(this, SignInActivity.class));
     }
 
