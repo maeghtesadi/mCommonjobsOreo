9c2c84c0d9af442b25de7653d5218aad4c367046
Kim Noel <kimcodes@gmail.com>
2017-03-15 18:38:19 -0400

    removed dead code from main acitivtY

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/MainActivity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/MainActivity.java
@@ -3,13 +3,10 @@ package com.oreo.mcommonjobs.Activtity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 
 import com.oreo.mcommonjobs.R;
 
 public class MainActivity extends AppCompatActivity {
-    EditText usernameInput;
-    EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
