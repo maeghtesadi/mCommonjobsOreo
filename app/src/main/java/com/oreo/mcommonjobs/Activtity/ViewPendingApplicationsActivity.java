f2428c69562b5a87ccb7b3152a81c0d8cd9d9b4a
jason <jtsalikis@hotmail.ca>
2017-04-03 04:58:10 -0400

    created viewpendingApplicationsActivity

---
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/ViewPendingApplicationsActivity.java
@@ -0,0 +1,20 @@
+package com.oreo.mcommonjobs.Activtity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.oreo.mcommonjobs.R;
+
+/**
+ * Created by jason on 2017-04-03.
+ */
+
+public class ViewPendingApplicationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pending_applications);
+
+    }
+}
