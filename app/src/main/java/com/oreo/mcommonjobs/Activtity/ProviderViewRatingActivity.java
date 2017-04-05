07fd321ba996eae4e35d2556c415f3dc40ecbcb3
Armine Iradian <armirad@yahoo.ca>
2017-04-04 22:03:25 -0700

    implemented providerViewRatingActivity

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/ProviderViewRatingActivity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/ProviderViewRatingActivity.java
@@ -2,8 +2,10 @@ package com.oreo.mcommonjobs.Activtity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.oreo.mcommonjobs.R;
+import com.oreo.mcommonjobs.Session.PersonSession;
 
 /**
  * This is the class for the provider viewing one of their ratings Activity.
@@ -14,9 +16,26 @@ import com.oreo.mcommonjobs.R;
 
 public class ProviderViewRatingActivity extends AppCompatActivity {
 
+    PersonSession personSession = PersonSession.getInstance();
+    TextView seekerName, avgrating, attitudeR, helpfulnessR, QualityR, comment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_view_rating);
+
+        seekerName = (TextView) findViewById(R.id.seekerName);
+        avgrating = (TextView) findViewById(R.id.avgRating);
+        attitudeR = (TextView) findViewById(R.id.rating1);
+        helpfulnessR = (TextView) findViewById(R.id.rating2);
+        QualityR = (TextView) findViewById(R.id.rating3);
+        comment = (TextView) findViewById(R.id.comment);
+
+        seekerName.setText(getIntent().getStringExtra("DISPNAME_EXTRA") + " " + getIntent().getStringExtra("LASTNAME_EXTRA"));
+        avgrating.setText(Double.toString(getIntent().getDoubleExtra("AVGR_EXTRA", 0.0)));
+        attitudeR.setText(getIntent().getIntExtra("R1_EXTRA", 0));
+        helpfulnessR.setText(getIntent().getIntExtra("R2_EXTRA", 0));
+        QualityR.setText(getIntent().getIntExtra("R3_EXTRA", 0));
+        comment.setText(getIntent().getStringExtra("COMMENT_EXTRA"));
     }
 }
