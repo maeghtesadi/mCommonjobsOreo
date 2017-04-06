3621e35fa6d537285b7d9fb3f672668d6346981b
jason <jtsalikis@hotmail.ca>
2017-04-05 20:14:01 -0400

    added account button to navigation activity for both jobseeker an                                                                                                                                                             d job provider, added layout for account activity

---
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/AccountActivity.java
@@ -0,0 +1,18 @@
+package com.oreo.mcommonjobs.Activtity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jason on 2017-04-05.
+ */
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
