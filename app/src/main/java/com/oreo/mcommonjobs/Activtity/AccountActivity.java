bb279525c338f369393fc1ad3834a7bd65d2a2fb
jason <jtsalikis@hotmail.ca>
2017-04-05 23:15:26 -0400

    initiated button template

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/AccountActivity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/AccountActivity.java
@@ -2,6 +2,9 @@ package com.oreo.mcommonjobs.Activtity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.oreo.mcommonjobs.R;
 
 /**
  * Created by jason on 2017-04-05.
@@ -9,10 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AccountActivity extends AppCompatActivity {
 
+
+    TextView firstNameTextView, lastNameTextView, emailTextView, phoneNumberTextView;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
 
+        firstNameTextView = (TextView) findViewById(R.id.textView_firstname);
+        lastNameTextView = (TextView) findViewById(R.id.textView_lastname);
+        emailTextView = (TextView) findViewById(R.id.textView_email);
+        phoneNumberTextView = (TextView) findViewById(R.id.textView_phonenumber);
 
     }
+
+
+
+
 }
