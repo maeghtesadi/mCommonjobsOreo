2a96104ec5e86fd2609455b4aa796a73806ffdde
jason <jtsalikis@hotmail.ca>
2017-04-06 01:21:24 -0400

    implented account button intent call to account in jobprovider nav                                                                                                                                                              class, also added method to get phone # and displayname from db to update the p                                                                                                                                                             erson instance

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/NavigationActivityForJobProvider.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/NavigationActivityForJobProvider.java
@@ -1,12 +1,27 @@
 package com.oreo.mcommonjobs.Activtity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.oreo.mcommonjobs.Models.URLPath;
 import com.oreo.mcommonjobs.R;
+import com.oreo.mcommonjobs.Session.PersonSession;
+import com.oreo.mcommonjobs.Session.RequestSingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This is the class for the Navigation Activity belonging to a JobProvider.
@@ -16,8 +31,8 @@ import com.oreo.mcommonjobs.R;
  */
 public class NavigationActivityForJobProvider extends AppCompatActivity {
 
-    Button btnAddJob;
-    Button viewApplicants;
+    Button btnAddJob ,viewApplicants , account;
+    PersonSession personInstance = PersonSession.getInstance();
 
     /**
      * Initializes the NavigationActivity for a JobProvider.
@@ -29,6 +44,19 @@ public class NavigationActivityForJobProvider extends AppCompatActivity {
         setContentView(R.layout.activity_navigation_jobprovider);
         btnAddJob = (Button) findViewById(R.id.btn_addjob);
         viewApplicants = (Button) findViewById(R.id.btn_view_applicants);
+        account = (Button) findViewById(R.id.btn_account);
+
+        getDisplayName_PhoneNumber(personInstance.getEmail(), getApplicationContext());
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AccountActivity.class);
+                startActivity(i);
+
+            }
+        });
+
 
         btnAddJob.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -47,5 +75,57 @@ public class NavigationActivityForJobProvider extends AppCompatActivity {
 
 
     }
+
+
+    /**
+     * This method checks gets the displayName and phone number of jobprovider from database
+     * @param email - email of the jobprovider
+     * @param context
+     */
+
+
+
+    public void getDisplayName_PhoneNumber(final String email, Context context){
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("email", email);
+
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, URLPath.getDisplayName, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        ;
+                        try {
+                            PersonSession personInstance = PersonSession.getInstance();
+                            personInstance.setDisplayName(response.getString("displayname"));
+
+                            if(response.getString("phoneNumber").equals("None")){
+                                personInstance.setPhoneNumber("Add your number");
+
+                            }else{
+                                personInstance.setPhoneNumber(response.getString("phoneNumber"));
+                                String test = response.getString("phoneNumber");
+                            }
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("Error", "Unable to parse json array");
+            }
+        });
+        RequestSingleton.getInstance(context).addToRequestQueue(jsonRequest);
+
+
+    }
+
+
+
 
 }
