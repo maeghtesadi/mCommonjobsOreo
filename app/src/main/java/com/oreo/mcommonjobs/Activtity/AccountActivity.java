7366387fdf6e59301c5561185080fe0aecf2905b
jason <jtsalikis@hotmail.ca>
2017-04-06 00:02:38 -0400

    Phone number button now reflects number stored in peronsinstance

--- app/src/main/java/com/oreo/mcommonjobs/Activtity/AccountActivity.java
+++ app/src/main/java/com/oreo/mcommonjobs/Activtity/AccountActivity.java
@@ -2,9 +2,12 @@ package com.oreo.mcommonjobs.Activtity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.oreo.mcommonjobs.R;
+import com.oreo.mcommonjobs.Session.PersonSession;
 
 /**
  * Created by jason on 2017-04-05.
@@ -12,17 +15,38 @@ import com.oreo.mcommonjobs.R;
 
 public class AccountActivity extends AppCompatActivity {
 
-
-    TextView firstNameTextView, lastNameTextView, emailTextView, phoneNumberTextView;
+    PersonSession personInstance = PersonSession.getInstance();
+    Button btn_editphonenumber, btn_editusername;
+    TextView firstNameTextView, lastNameTextView, emailTextView, phoneNumberTextView, usernameTextView;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
         firstNameTextView = (TextView) findViewById(R.id.textView_firstname);
+        firstNameTextView.setText(personInstance.getFirstName());
+
         lastNameTextView = (TextView) findViewById(R.id.textView_lastname);
+        lastNameTextView.setText(personInstance.getLastName());
+
         emailTextView = (TextView) findViewById(R.id.textView_email);
-        phoneNumberTextView = (TextView) findViewById(R.id.textView_phonenumber);
+        emailTextView.setText(personInstance.getEmail());
+
+        phoneNumberTextView = (EditText) findViewById(R.id.textView_phonenumber);
+        phoneNumberTextView.setText(personInstance.getPhoneNumber());
+
+
+        usernameTextView = (EditText) findViewById(R.id.textView_username);
+        usernameTextView.setText(personInstance.getDisplayName());
+
+
+
+
+
+        btn_editphonenumber= (Button)  findViewById(R.id.btn_edit_phonenumber);
+        btn_editusername= (Button)  findViewById(R.id.btn_edit_username);
+
+
 
     }
 
