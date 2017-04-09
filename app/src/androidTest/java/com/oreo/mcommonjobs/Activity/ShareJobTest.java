37e7a261c5d92676e148a82af3d24aa270c41057
Rameen Rastan <rameenrastanv@hotmail.com>
2017-04-09 15:58:08 -0400

    Added ShareJobTest

---
+++ app/src/androidTest/java/com/oreo/mcommonjobs/Activity/ShareJobTest.java
@@ -0,0 +1,109 @@
+package com.oreo.mcommonjobs.Activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.oreo.mcommonjobs.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ShareJobTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shareJobTest() {
+        ViewInteraction rc = onView(
+                allOf(withText("Sign in"),
+                        withParent(withId(R.id.sign_in_button)),
+                        isDisplayed()));
+        rc.perform(click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        withId(R.id.viewprofiles),
+                        4),
+                        isDisplayed()));
+        linearLayout.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_addjob), withText("View All Jobs"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction relativeLayout = onView(
+                allOf(childAtPosition(
+                        withId(R.id.joblist),
+                        0),
+                        isDisplayed()));
+        relativeLayout.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btnShare), withText("Share"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText.perform(click());
+
+        ViewInteraction editText2 = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText2.perform(replaceText("teamoreo.2017@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(android.R.id.button1), withText("SHARE")));
+        appCompatButton3.perform(scrollTo(), click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
